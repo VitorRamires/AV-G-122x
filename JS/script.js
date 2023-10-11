c9decd54d8fe3df78a9233af790802ee6f6709d1
VitorRamires <vmr.rs@hotmail.com>
2023-10-11 10:33:55 -0300

    js tabs

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+/* tabs */
+
+const tabs = document.querySelectorAll(".tab")
+const textos = document.querySelectorAll('.texto')
+
+function handleActiveTabs(index){
+  tabs.forEach(tab=>{
+    tab.classList.remove('active-tab')
+    tab.classList.add('desactive-tab')
+  })
+  textos.forEach(texto=>{
+    texto.classList.remove('active-texto')
+  })
+  tabs[index].classList.add('active-tab')
+  textos[index].classList.add('active-texto')
+}
+
+tabs.forEach((tab, index)=>{
+  tab.addEventListener("click", ()=>{
+    handleActiveTabs(index)
+  })
+})
+
+/* srcoll */
+
