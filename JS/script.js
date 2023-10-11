7cbcc43f210b9e4ae18f1634e864813d45cc042c
VitorRamires <vmr.rs@hotmail.com>
2023-10-11 15:27:29 -0300

    numbers animation

--- JS/script.js
+++ JS/script.js
@@ -64,3 +64,25 @@ tabs.forEach((tab, index)=>{
   mapa.addEventListener('mousemove', handleTooltipMove)
   mapa.addEventListener('mouseover', handleTooltipHover)
   mapa.addEventListener('mouseleave', handleTooltipLeave)
+
+  /* numeros */
+
+  const numeros = Array.from(document.querySelectorAll("spam"))
+  
+  numeros.forEach(num=>{
+    const numbers = Number.parseFloat(num.innerText)
+    const numbersInterval = setInterval(handleNumber, 10)
+    let timer = 0
+    let incremento = Math.floor(numbers / 90)
+    
+    function handleNumber(){
+      timer += incremento
+      num.innerHTML = timer
+      if(timer >= numbers){
+        num.innerHTML = numbers
+        clearInterval(numbersInterval)
+      }
+    }
+  })
+
+  
