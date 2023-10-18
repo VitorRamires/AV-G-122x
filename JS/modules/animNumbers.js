89939a532f1e74609dd2a369ffb8d4e91b0b3b24
VitorRamires <vmr.rs@hotmail.com>
2023-10-18 16:46:31 -0300

    melhorando animações

--- JS/modules/animNumbers.js
+++ JS/modules/animNumbers.js
@@ -1,20 +1,38 @@
 
 export default function animNumbers(){
   const numeros = Array.from(document.querySelectorAll("spam"))
-    
-  numeros.forEach(num=>{
-    const numbers = Number.parseFloat(num.innerText)
-    const numbersInterval = setInterval(handleNumber, 10)
-    let timer = 0
-    let incremento = Math.floor(numbers / 90)
-    
-    function handleNumber(){
-      timer += incremento
-      num.innerHTML = timer
-      if(timer >= numbers){
-        num.innerHTML = numbers
-        clearInterval(numbersInterval)
+ 
+  function animationRandomNumbers(){  
+    numeros.forEach(num=>{
+      const numbers = Number.parseFloat(num.innerText)
+      const numbersInterval = setInterval(handleNumber, 10)
+      let timer = 0
+      let incremento = Math.floor(numbers / 90)
+      
+      function handleNumber(){
+        timer += incremento
+        num.innerHTML = timer
+        if(timer >= numbers){
+          num.innerHTML = numbers
+          clearInterval(numbersInterval)
+        }
       }
+    })
+  }
+  
+  function observerHandle(mutation){
+    if(mutation[0].target.classList.contains("active")){
+      observerAction.disconnect()
+      animationRandomNumbers()
     }
-  })
+  }
+
+  const destaqueSection = document.querySelector(".destaque")
+  const observerAction = new MutationObserver(observerHandle)
+
+  observerAction.observe(destaqueSection, {attributes:true})
+
+
+
 }
+
--- JS/modules/scrollAnim.js
+++ JS/modules/scrollAnim.js
@@ -1,15 +1,22 @@
 export default function scrollAnime(){
-  const animeElements = document.querySelectorAll('.scrollAnime')
+  const animeElements = document.querySelectorAll('.anime-element')
   const windowHeight = window.innerHeight * 0.6
+  const bodyDocument = document.querySelector('body')
 
   function scrollAnimeHandle(){
     animeElements.forEach(elements=>{
       const topDistance = elements.getBoundingClientRect().top
       if(topDistance < windowHeight){
-        elements.classList.add('scrollOn')
+        elements.classList.add('active')
+      }else if(topDistance >= windowHeight) {
+        elements.classList.remove('active')
       }
     })
   }
 
+  scrollAnimeHandle()
+
   window.addEventListener('scroll', scrollAnimeHandle)
+
 }
+
