159ddfb7506a14df5a581184df5281dd5e2a8017
VitorRamires <vmr.rs@hotmail.com>
2023-10-19 11:18:58 -0300

    mobile inicio

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,10 @@
 import scrollAnime from "./modules/scrollAnim.js"
+import navTab from "./modules/tabs.js"
 import accordionList from './modules/FAQ.js'
 import animNumbers from "./modules/animNumbers.js"
 import tooltipMapHandler from "./modules/tooltipMap.js"
 import smoothScroll from "./modules/smoothScroll.js"
-import navTab from "./modules/tabs.js"
+import mobile from "./modules/mobile.js"
 
 scrollAnime()
 accordionList()
@@ -11,7 +12,7 @@ animNumbers()
 tooltipMapHandler()
 smoothScroll()
 navTab()
-
+mobile()
 
 
 
