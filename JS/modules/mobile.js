acfc076e4213d6269b49b8ff00873a9829d38941
VitorRamires <vmr.rs@hotmail.com>
2023-10-20 11:22:18 -0300

    mobile menu + modelos

--- JS/modules/mobile.js
+++ JS/modules/mobile.js
@@ -2,16 +2,17 @@ export default function mobile(){
 
   const mobileMenuIcon = document.querySelector(".menu-mobile-icon")
   const menu = document.querySelector(".menu")
+  const barsMenu = document.querySelectorAll('.bar')
 
   function handleMenuMobile(){
-    if(!menu.classList.contains("menuOn")){
-      menu.classList.add("menuOn")
-    } else {
-      menu.classList.remove("menuOn")
+    if(!menu.classList.contains('menuOn')){
+      menu.classList.add('menuOn')
+ 
+    } else if (menu.classList.contains('menuOn')) {
+      menu.classList.remove('menuOn')
+
     }
   }
 
   mobileMenuIcon.addEventListener('click', handleMenuMobile)
-  
-
 }
