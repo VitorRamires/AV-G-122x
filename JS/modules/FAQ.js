cef19635966292d359569cc2b1697a4449e0a05f
VitorRamires <vmr.rs@hotmail.com>
2023-10-23 23:07:41 -0300

    faq + smooth

--- JS/modules/FAQ.js
+++ JS/modules/FAQ.js
@@ -1,23 +1,15 @@
 
 export default function accordionList(){
   const questions = document.querySelectorAll('.FAQ-body')
-  const respostas = document.querySelectorAll('.FAQ-body .resposta')
+  const respostas = document.querySelectorAll('.resposta p')
   const faqDecoration = document.querySelectorAll(".FAQ-decoration")
 
   function handleAccordionList(index){
-    
-    respostas.forEach(resposta=>{
-      resposta.classList.remove('active-accordion')
-    })
-    faqDecoration.forEach(decoration=>{
-    decoration.classList.remove('anime-height')
-    })
-
-    respostas[index].classList.add('active-accordion')
-    faqDecoration[index].classList.add('anime-height')
+      respostas[index].classList.toggle('active-accordion')
+      faqDecoration[index].classList.toggle('anime-height')
   }
 
-  questions.forEach((question, index)=>{
+  questions.forEach((question, index) => {
     question.addEventListener('click', ()=>{
       handleAccordionList(index)
     })
