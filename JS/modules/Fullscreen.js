9e5fc228098b803b2220e2a95b5b7af425970be4
VitorRamires <vmr.rs@hotmail.com>
2023-10-31 11:31:58 -0300

    ajustando galeria

--- JS/modules/Fullscreen.js
+++ JS/modules/Fullscreen.js
@@ -3,13 +3,13 @@ export default function fullScreenMode(){
   const fotos = document.querySelectorAll('.foto img')
   const fullScreenDisplay = document.querySelector('.fullscreen-foto')
   const fullScreenActive = document.querySelector('.fullscreen-mode')
-  const closeBtn = document.querySelector('.closeBtn')
+  const closeBtn = document.querySelector('.closeBtn img')
 
 
   function clickOutside(event){
     const clickTargetElement = event.target
     
-    if(clickTargetElement.classList.contains("fullOn") || closeBtn){
+    if(clickTargetElement === fullScreenActive || clickTargetElement === closeBtn){
       fullScreenActive.classList.remove("fullOn")
     }
   }
@@ -24,11 +24,13 @@ export default function fullScreenMode(){
   }
 
 
-  fotos.forEach((foto, index)=>{
-    foto.addEventListener('click', ()=>{
-      handleFullscreen(index)
+  if(window.matchMedia('(min-width:650px)').matches){
+    fotos.forEach((foto, index)=>{
+      foto.addEventListener('click', ()=>{
+        handleFullscreen(index)
+      })
     })
-  })
+  }
 
 
 }
