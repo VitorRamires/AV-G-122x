5767d8f937dd53bacb2a40431e08d321c175727e
VitorRamires <vmr.rs@hotmail.com>
2023-11-14 20:08:31 -0300

    ajustes

--- JS/modules/smoothScroll.js
+++ JS/modules/smoothScroll.js
@@ -7,9 +7,10 @@ export default function smoothScroll(){
     const currentLink = event.target.getAttribute('id')
     const actualSection = document.querySelector(`.${currentLink}`)
     window.scrollTo({ 
-      top: actualSection.getBoundingClientRect().top - 100,
+      top: actualSection.getBoundingClientRect().top,
       behavior:'smooth'
     })
+    console.log(actualSection.getBoundingClientRect().top)
   }
 
   links.forEach(link=>{
