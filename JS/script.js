60375e2ad38af6e50790edd64d707472571b77e0
VitorRamires <vmr.rs@hotmail.com>
2023-11-21 16:48:51 -0300

    mudando scroll

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,6 @@ import scrollAnime from "./modules/scrollAnim.js"
 import navTab from "./modules/tabs.js"
 import accordionList from './modules/FAQ.js'
 import animNumbers from "./modules/animNumbers.js"
-import smoothScroll from "./modules/smoothScroll.js"
 import menu from "./modules/menu.js"
 import fullScreenMode from "./modules/Fullscreen.js"
 import mapHandle from "./modules/map.js"
@@ -10,7 +9,6 @@ import mapHandle from "./modules/map.js"
 scrollAnime()
 accordionList()
 animNumbers()
-smoothScroll()
 navTab()
 menu()
 fullScreenMode()
